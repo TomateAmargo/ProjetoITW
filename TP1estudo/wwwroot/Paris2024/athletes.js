3b38f162aedf0260c82d47b60648450857c75d97
TomateAmargo <nuno27066@gmail.com>
2024-12-03 15:15:50 +0000

    Merdinhas que eu fiz na aula de ITW

--- TP1estudo/wwwroot/Paris2024/athletes.js
+++ TP1estudo/wwwroot/Paris2024/athletes.js
@@ -59,6 +59,11 @@ var vm = function () {
             //self.SetFavourites();
         });
     };
+    self.viewDetails = function (athlete) {
+        // Redirect to the details page with the athlete's ID
+        window.location.href = `Athletes/Details/${athlete.Id}`;
+    };
+
 
     //--- Internal functions
     function ajaxHelper(uri, method, data) {
