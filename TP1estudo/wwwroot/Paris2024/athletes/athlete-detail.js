484180405ca89227fb0454fcd7eac94d9cf9361e
TomateAmargo <144570180+TomateAmargo@users.noreply.github.com>
2024-12-04 19:12:21 +0000

    Detalhes Atletas etc.
    Adiciona a página de detalhes dos atletas, html e js, e altera para que as rows brilhem ao dar hover nelas.
    "Conserta" as barras de loading

---
+++ TP1estudo/wwwroot/Paris2024/athletes/athlete-detail.js
@@ -0,0 +1,77 @@
+var vm = function () {
+    console.log('ViewModel initiated...');
+    var self = this;
+
+    // Observables
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/API/athletes');
+    self.displayName = ko.observable('Athlete Detail');
+    self.error = ko.observable('');
+    self.Name = ko.observable('');
+    self.BirthDate = ko.observable('');
+    self.BirthPlace = ko.observable('');
+    self.Sex = ko.observable('');
+    self.AdditionalInfo = ko.observable('');
+
+    // Function to fetch athlete details
+    self.activate = function (id) {
+        console.log('CALL: getAthlete...');
+        const composedUri = `${self.baseUri()}/${id}`;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            hideLoading();
+            self.Name(data.Name);
+            self.BirthDate(data.BirthDate);
+            self.BirthPlace(data.BirthPlace);
+            self.Sex(data.Sex);
+            self.AdditionalInfo(data.AdditionalInfo || 'No additional information available.');
+        });
+    };
+
+    // AJAX helper
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error(`AJAX Call [${uri}] Fail:`, errorThrown);
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    // Loading indicators
+    function showLoading() {
+        $("#myModal").modal('show', { backdrop: 'static', keyboard: false });
+    }
+
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        });
+    }
+
+    // Get athlete ID from URL
+    function getUrlParameter(param) {
+        const params = new URLSearchParams(window.location.search);
+        return params.get(param);
+    }
+
+    // Initialization
+    showLoading();
+    const athleteId = getUrlParameter('id');
+    if (athleteId) {
+        self.activate(athleteId);
+    } else {
+        console.error('Athlete ID not found in URL');
+        hideLoading();
+    }
+};
+
+$(document).ready(function () {
+    console.log("Document ready!");
+    ko.applyBindings(new vm());
+});
