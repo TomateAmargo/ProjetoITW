692dd1cb4c4ef2442c2e353074b5f762f5cff9a0
souofrancisco <franciscocorreia@ua.pt>
2024-12-14 14:28:30 +0000

    o resto ta feito fio
    só falta fazer os detalhes

---
+++ TP1estudo/wwwroot/Paris2024/comites/comites.js
@@ -0,0 +1,92 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    var self = this;
+
+    // Observáveis
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/API/NOCs');
+    self.displayName = 'Paris2024 NOCs List';
+    self.error = ko.observable('');
+    self.comites = ko.observableArray([]);
+    self.currentPage = ko.observable(1);
+    self.pagesize = ko.observable(20);
+    self.totalRecords = ko.observable(50);
+    self.hasPrevious = ko.observable(false);
+    self.hasNext = ko.observable(false);
+    self.totalPages = ko.observable(0);
+
+    // Computed observables
+    self.previousPage = ko.computed(() => Math.max(1, self.currentPage() - 1));
+    self.nextPage = ko.computed(() => Math.min(self.totalPages(), self.currentPage() + 1));
+
+    self.fromRecord = ko.computed(() => (self.currentPage() - 1) * self.pagesize() + 1);
+    self.toRecord = ko.computed(() => Math.min(self.currentPage() * self.pagesize(), self.totalRecords()));
+
+    self.pageArray = function () {
+        const size = Math.min(self.totalPages(), 9);
+        const step = Math.max(0, Math.min(self.currentPage() - 5, self.totalPages() - 9));
+        return Array.from({ length: size }, (_, i) => i + 1 + step);
+    };
+
+    // Função para ativar a página
+    self.activate = function (id) {
+        console.log('CALL: getCoaches...');
+        const composedUri = `${self.baseUri()}?page=${id}&pageSize=${self.pagesize()}`;
+        showLoading();
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            hideLoading();
+            self.comites(data.NOCs); 
+            self.currentPage(data.CurrentPage);
+            self.hasNext(data.HasNext);
+            self.hasPrevious(data.HasPrevious);
+            self.pagesize(data.PageSize);
+            self.totalPages(data.TotalPages);
+            self.totalRecords(data.TotalNOCs); 
+        });
+    };
+
+    // Função AJAX
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error(`AJAX Call [${uri}] Fail:`, errorThrown);
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    // Funções para mostrar e esconder o loading
+    function showLoading() {
+        $("#myModal").modal('show', { backdrop: 'static', keyboard: false });
+    }
+
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    // Função para obter parâmetros da URL
+    function getUrlParameter(sParam) {
+        const params = new URLSearchParams(window.location.search);
+        return params.get(sParam);
+    }
+
+    // Inicialização
+    showLoading();
+    const page = getUrlParameter('page') || 1;
+    self.activate(page);
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("Document ready!");
+    ko.applyBindings(new vm());
+});
---
+++ TP1estudo/wwwroot/Paris2024/medals/medals.js
@@ -0,0 +1,67 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    var self = this;
+
+    // Observáveis
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/API/medals');
+    self.displayName = 'Paris2024 Medals List';
+    self.error = ko.observable('');
+    self.medals = ko.observableArray([]); 
+
+    // Função para ativar a página
+    self.activate = function (id) {
+        console.log('CALL: getmedals...');
+        const composedUri = `${self.baseUri()}?page=${id}&pageSize=${self.pagesize()}`;
+        showLoading();
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            hideLoading();
+            self.medals(data.Medals); 
+        });
+    };
+
+    // Função AJAX
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error(`AJAX Call [${uri}] Fail:`, errorThrown);
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    // Funções para mostrar e esconder o loading
+    function showLoading() {
+        $("#myModal").modal('show', { backdrop: 'static', keyboard: false });
+    }
+
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    // Função para obter parâmetros da URL
+    function getUrlParameter(sParam) {
+        const params = new URLSearchParams(window.location.search);
+        return params.get(sParam);
+    }
+
+    // Inicialização
+    showLoading();
+    const page = getUrlParameter('page') || 1;
+    self.activate(page);
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("Document ready!");
+    ko.applyBindings(new vm());
+});
