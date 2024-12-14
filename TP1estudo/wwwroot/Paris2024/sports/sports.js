593d0dc104fba9b9a7152244b8b1506593ae1714
souofrancisco <franciscocorreia@ua.pt>
2024-12-14 14:36:39 +0000

    ultima pagina (sports)
    adicionei a ultima pagina aqui nuno

---
+++ TP1estudo/wwwroot/Paris2024/sports/sports.js
@@ -0,0 +1,61 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    var self = this;
+
+    // Observáveis
+    self.baseUri = ko.observable('http://192.168.160.58/Paris2024/api/sports');
+    self.displayName = 'Paris2024 Sports List';
+    self.error = ko.observable('');
+    self.sports = ko.observableArray(''); 
+
+    // Função para ativar a página
+    self.activate = function () {
+        console.log('CALL: getMedals...');
+        const composedUri = self.baseUri();
+        showLoading();
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            hideLoading(); 
+            console.log(data);
+            self.sports(data)
+        });
+    };
+
+    // Função AJAX
+    function ajaxHelper(uri, method, data) {
+        self.error('');
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error(`AJAX Call [${uri}] Fail:`, errorThrown);
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    // Funções para mostrar e esconder o loading
+    function showLoading() {
+        $("#myModal").modal('show', { backdrop: 'static', keyboard: false });
+    }
+
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    // Inicialização
+    showLoading();
+    self.activate();
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("Document ready!");
+    ko.applyBindings(new vm());
+});
